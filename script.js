e2c037a18dbac93a1d8ccddc35b3b84bc14a5c2b
Isabella <iasbellaporraslopez@gmail.com>
2024-02-19 14:57:50 -0500

    clored grid and disconnected range bar

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const container = document.querySelector('.container');
 
+
+function getValue() {
+        const range = document.querySelector('#size');
+        var size = range.value;
+
+        console.log(size);
+}
+
 for(i = 0; i < 256; i++) {
-        const div = document.createElement("div");
-        div.setAttribute('style','height: 20px; width: 20px; background-color: blue; background: white;');
-        container.appendChild(div);
+        const sqrs = document.createElement("div");
+        sqrs.setAttribute('style','height: 30px; width: 30px; background: grey;');
+        sqrs.addEventListener("mouseover", () => {
+                sqrs.style.background = 'blue';
+        })
+        container.appendChild(sqrs);
 }
